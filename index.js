0754a943f10081490cb6e688ccabce286d5ce382
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-06-30 11:19:36 -0500

    Updated js to write to readme file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require("inquirer")
 const fs = require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
+inquirer.prompt([
     {
         message: "What is the title of your application?",
         name: "title",
@@ -32,7 +32,7 @@ const questions = inquirer.prompt([
         message: "Plese select your application's license below:",
         name: "license",
         type: "list",
-        options: ['Apache License 2.0', 'GNU General Puplic License v3.0', 'MIT License', 'BSD 2-Clause Simplified License', 'BSD 3-Clause New or Revised License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+        choices: ['None','Apache License 2.0', 'GNU General Puplic License v3.0', 'MIT License', 'BSD 2-Clause Simplified License', 'BSD 3-Clause New or Revised License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
     },
     {
         message: "Please provide your GitHub username:",
@@ -42,13 +42,44 @@ const questions = inquirer.prompt([
         message: "Please provide your email address:",
         name: "email",
     },
-]);
+]).then(response =>
+    fs.appendFile('README.md', writeToFile(response), err =>
+        err ? console.error('Failed to write to file') : console.log('Success!'))
+);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(response) {
+    return `
+# Title
+${response.title}
+
+## Description
+${response.description}
+
+## Table of Contents
+
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
+
+## License
+${response.license}
+
+## Contributing
+${response.guidelines}
+
+## Tests
+${response.test}
+
+## Questions
+Find me on GitHub: ${response.username}
+For further questions: ${response.email}`
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() { }
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
